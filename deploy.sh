ff5be08a2f3e6710c65d0b7882a291d00a34bd13
Lucas Costa <lucasrc.rodri@gmail.com>
2023-05-16 21:14:54 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,15 @@
 #git config credential.helper store
 
 # Change directory to app folder
-cd /home/lucasrc/cotecforms
+cd /home/user/myapp
 
 # Deployment execution
 echo "Shutting down containers"
 docker-compose down
 
 echo "Pulling new code from repository"
+export HOME=/home/user
+git config --global --add safe.directory /home/user/myapp
 git pull
 
 echo "Building new containers"
